2348fa96411825a55329156f1b8efc6c4aad67ee
alxayeed <alxayeed@gmail.com>
2022-01-11 11:13:39 +0600

    project initialized

---
+++ src/test/java/com/alxayeed/restservices/restfulspring/RestfulSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alxayeed.restservices.restfulspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
